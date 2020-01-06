1e721fd4fed1cd0c581bc5e424464c123e53c484
ParulPrasad <59188895+ParulPrasad@users.noreply.github.com>
2020-01-06 14:55:59 +0530

    adding values

---
+++ myScript.js
@@ -0,0 +1,7 @@
+ function add(){
+     var a=parseInt(document.getElementById("t1").value);
+     var b=parseInt(document.getElementById("t2").value);
+     var sum=a+b;
+     document.getElementById("t3").value=sum;
+
+ }
