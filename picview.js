e65e10a07cdb9bb376b981e0e2f8c21a7ddaadd0
ParulPrasad <59188895+ParulPrasad@users.noreply.github.com>
2020-01-07 09:49:53 +0530

    Update picview.js

--- picview.js
+++ picview.js
@@ -3,6 +3,8 @@ var img=document.getElementById("image");
 var imgs=["img7.jpg","img8.jpg","img10.jpg"];
 var count=0;
 
+setInterval(()=>next(),2000);   //can also pass the whole next() function i.e function body
+
 function next(){
     if(count==imgs.length-1){
         count=-1;
